93f050144a4e755660213d2819c465921723817b
Shigeya Suzuki <shigeya@wide.ad.jp>
2012-09-27 19:27:38 +0900

    use of require_relative

--- lib/markdown_to_tex.rb
+++ lib/markdown_to_tex.rb
@@ -1,3 +1,3 @@
-require 'markdown_to_tex/renderer'
-require 'markdown_to_tex/text_processor'
-require 'markdown_to_tex/processor'
+require_relative 'markdown_to_tex/renderer'
+require_relative 'markdown_to_tex/text_processor'
+require_relative 'markdown_to_tex/processor'
